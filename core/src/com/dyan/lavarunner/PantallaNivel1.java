7a83dab6d17cf33bbbb3bdc84c945f43e8a83a7f
daniel5115 <daniel811815@gmail.com>
2021-03-14 22:42:01 -0600

    Configuracion

--- core/src/com/dyan/lavarunner/PantallaNivel1.java
+++ core/src/com/dyan/lavarunner/PantallaNivel1.java
@@ -1,21 +1,21 @@
 package com.dyan.lavarunner;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
-
 /*
- * Autor: C.Daniel Alvarez C.
- * */
-public class PantallaConfiguracion extends Pantalla{
-    //Referencia al juego principal
+*
+*
+*
+ */
+public class PantallaNivel1 extends Pantalla {
+
+    //
     private Juego juego;
     //Fondo
     private Texture texturaFondo;
@@ -23,27 +23,26 @@ public class PantallaConfiguracion extends Pantalla{
     private Stage escenaMenu;
 
 
-    public PantallaConfiguracion(Juego juego) {
+    public PantallaNivel1(Juego juego) {
         this.juego = juego;
     }
-
     @Override
     public void show() {
-        crearConfig();
+
+        crearNivel1();
 
     }
-    private void crearConfig() {
 
+    private void crearNivel1() {
         escenaMenu=new Stage(vista);
+        texturaFondo=new Texture("menuNiveles/nivel1/fondoNivel1jpg");
 
 
-        texturaFondo=new Texture("configuracion/fondoConfiguracion.jpg");
-
-        Button btnRegresar=crearBoton("configuracion/btn_regresar.png");
-        btnRegresar.setPosition(ANCHO/5,ALTO/5, Align.center);
-        escenaMenu.addActor(btnRegresar);
+        Button btnMenu=crearBoton("menuNiveles/nivel1/button_menu.png");
+        btnMenu.setPosition(10,ALTO, Align.center);
+        escenaMenu.addActor(btnMenu);
         //Registrar el evento de click para el boton
-        btnRegresar.addListener(new ClickListener(){
+        btnMenu.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 //Cambiar pantalla
@@ -51,19 +50,9 @@ public class PantallaConfiguracion extends Pantalla{
             }
         });
 
-        Button btnMute=crearBoton("configuracion/btnMute.png");
-        btnMute.setPosition(ANCHO/2,ALTO/1.8f, Align.center);
-        escenaMenu.addActor(btnMute);
-        //Registrar el evento de click para el boton
-        btnMute.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                //Cambiar pantalla
-                //juego.setScreen(new PantallaMenu(juego));
-            }
-        });
         //ESCENA SE ENCARGA DE ATENDER LOS EVENTOS DE ENTRADA
         Gdx.input.setInputProcessor(escenaMenu);
+
     }
 
     private Button crearBoton(String archivo) {
